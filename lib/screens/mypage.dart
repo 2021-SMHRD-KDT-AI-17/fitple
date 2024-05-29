84054ddde0c6c12c03f0787f6a065cee469d8d3c
SMHRD <ksw102180@gmail.com>
2024-05-29 16:33:31 +0900

    마이페이지 나의리뷰 수정

--- lib/screens/mypage.dart
+++ lib/screens/mypage.dart
@@ -1,3 +1,5 @@
+import 'package:fitple/screens/review_my.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,7 +12,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.light(), // 기본 테마를 밝은 테마로 설정합니다.
       home: Scaffold(
         body: MyPage(),
       ),
@@ -165,229 +166,262 @@ class Name extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Container(
-          width: 375,
-          height: 49,
-          padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 14),
-          clipBehavior: Clip.antiAlias,
-          decoration: BoxDecoration(),
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                width: 100,
-                child: Text(
-                  '회원 정보 수정',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 15,
-                    fontFamily: 'Inter',
-                    fontWeight: FontWeight.w500,
-                    height: 1.5,
+        GestureDetector(
+          onTap: () {
+            // 여기에 '회원 정보 수정'이 탭되었을 때 실행할 동작 추가
+          },
+          child: Container(
+            width: 375,
+            height: 49,
+            padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 14),
+            clipBehavior: Clip.antiAlias,
+            decoration: BoxDecoration(),
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  width: 100,
+                  child: Text(
+                    '회원 정보 수정',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 15,
+                      fontFamily: 'Inter',
+                      fontWeight: FontWeight.w500,
+                      height: 1.5,
+                    ),
                   ),
                 ),
-              ),
-              const SizedBox(width: 12),
-              Expanded(
-                child: Container(
-                  height: 20,
-                  child: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Opacity(
-                        opacity: 0.50,
-                        child: Container(
-                          width: 20,
-                          height: 20,
-                          padding: const EdgeInsets.only(
-                              top: 4.96, left: 14, bottom: 4.90),
-                          clipBehavior: Clip.antiAlias,
-                          decoration: BoxDecoration(),
-                          child: Row(
-                            mainAxisSize: MainAxisSize.min,
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [],
+                const SizedBox(width: 12),
+                Expanded(
+                  child: Container(
+                    height: 20,
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Opacity(
+                          opacity: 0.50,
+                          child: Container(
+                            width: 20,
+                            height: 20,
+                            padding: const EdgeInsets.only(
+                                top: 4.96, left: 14, bottom: 4.90),
+                            clipBehavior: Clip.antiAlias,
+                            decoration: BoxDecoration(),
+                            child: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [],
+                            ),
                           ),
                         ),
-                      ),
-                    ],
+                        Icon(Icons.keyboard_arrow_right_outlined),
+                      ],
+                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
-        Container(
-          width: 375,
-          height: 49,
-          padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 14),
-          clipBehavior: Clip.antiAlias,
-          decoration: BoxDecoration(),
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                width: 100,
-                child: Text(
-                  'PT 예약 내역',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 15,
-                    fontFamily: 'Inter',
-                    fontWeight: FontWeight.w500,
-                    height: 1.5,
+        GestureDetector(
+          onTap: () {
+            // 여기에 'PT 예약 내역'이 탭되었을 때 실행할 동작 추가
+          },
+          child: Container(
+            width: 375,
+            height: 49,
+            padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 14),
+            clipBehavior: Clip.antiAlias,
+            decoration: BoxDecoration(),
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  width: 100,
+                  child: Text(
+                    'PT 예약 내역',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 15,
+                      fontFamily: 'Inter',
+                      fontWeight: FontWeight.w500,
+                      height: 1.5,
+                    ),
                   ),
                 ),
-              ),
-              const SizedBox(width: 12),
-              Expanded(
-                child: Container(
-                  height: 20,
-                  child: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Opacity(
-                        opacity: 0.50,
-                        child: Container(
-                          width: 20,
-                          height: 20,
-                          padding: const EdgeInsets.only(
-                              top: 4.96, left: 14, bottom: 4.90),
-                          clipBehavior: Clip.antiAlias,
-                          decoration: BoxDecoration(),
-                          child: Row(
-                            mainAxisSize: MainAxisSize.min,
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [],
+                const SizedBox(width: 12),
+                Expanded(
+                  child: Container(
+                    height: 20,
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Opacity(
+                          opacity: 0.50,
+                          child: Container(
+                            width: 20,
+                            height: 20,
+                            padding: const EdgeInsets.only(
+                                top: 4.96, left: 14, bottom: 4.90),
+                            clipBehavior: Clip.antiAlias,
+                            decoration: BoxDecoration(),
+                            child: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [],
+                            ),
                           ),
                         ),
-                      ),
-                    ],
+                        Icon(Icons.keyboard_arrow_right_outlined),
+                      ],
+                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
-        Container(
-          width: 375,
-          height: 49,
-          padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 14),
-          clipBehavior: Clip.antiAlias,
-          decoration: BoxDecoration(),
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                width: 100,
-                child: Text(
-                  'My 리뷰',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 15,
-                    fontFamily: 'Inter',
-                    fontWeight: FontWeight.w500,
-                    height: 1.5,
+        GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) =>Review_my()),
+            );
+          },
+          child: Container(
+            width: 375,
+            height: 49,
+            padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 14),
+            clipBehavior: Clip.antiAlias,
+            decoration: BoxDecoration(),
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  width: 100,
+                  child: Text(
+                    'My 리뷰',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 15,
+                      fontFamily: 'Inter',
+                      fontWeight: FontWeight.w500,
+                      height: 1.5,
+                    ),
                   ),
                 ),
-              ),
-              const SizedBox(width: 12),
-              Expanded(
-                child: Container(
-                  height: double.infinity,
-                  child: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Opacity(
-                        opacity: 0.50,
-                        child: Container(
-                          width: 20,
-                          height: 20,
-                          padding: const EdgeInsets.only(
-                              top: 4.96, left: 14, bottom: 4.90),
-                          clipBehavior: Clip.antiAlias,
-                          decoration: BoxDecoration(),
-                          child: Row(
-                            mainAxisSize: MainAxisSize.min,
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [],
+                const SizedBox(width: 12),
+                Expanded(
+                  child: Container(
+                    height: double.infinity,
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Opacity(
+                          opacity: 0.50,
+                          child: Container(
+                            width: 20,
+                            height: 20,
+                            padding: const EdgeInsets.only(
+                                top: 4.96, left: 14, bottom: 4.90),
+                            clipBehavior: Clip.antiAlias,
+                            decoration: BoxDecoration(),
+                            child: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [],
+                            ),
                           ),
                         ),
-                      ),
-                    ],
+                        Icon(Icons.keyboard_arrow_right_outlined),
+                      ],
+                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
-        Container(
-          width: 375,
-          height: 49,
-          padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 14),
-          clipBehavior: Clip.antiAlias,
-          decoration: BoxDecoration(),
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                width: 100,
-                child: Text(
-                  '회원탈퇴',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 15,
-                    fontFamily: 'Inter',
-                    fontWeight: FontWeight.w500,
-                    height: 1.5,
+        GestureDetector(
+          onTap: () {
+            // 여기에 '회원탈퇴'가 탭되었을 때 실행할 동작 추가
+          },
+          child: Container(
+            width: 375,
+            height: 49,
+            padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 14),
+            clipBehavior: Clip.antiAlias,
+            decoration: BoxDecoration(),
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  width: 100,
+                  child: Text(
+                    '회원탈퇴',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 15,
+                      fontFamily: 'Inter',
+                      fontWeight: FontWeight.w500,
+                      height: 1.5,
+                    ),
                   ),
                 ),
-              ),
-            ],
+                Icon(Icons.keyboard_arrow_right_outlined),
+              ],
+            ),
           ),
         ),
-        Container(
-          width: 375,
-          height: 49,
-          padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 14),
-          clipBehavior: Clip.antiAlias,
-          decoration: BoxDecoration(),
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                width: 100,
-                child: Text(
-                  '로그아웃',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 15,
-                    fontFamily: 'Inter',
-                    fontWeight: FontWeight.w500,
-                    height: 1.5,
+        GestureDetector(
+          onTap: () {
+            // 여기에 '로그아웃'이 탭되었을 때 실행할 동작 추가
+          },
+          child: Container(
+            width: 375,
+            height: 49,
+            padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 14),
+            clipBehavior: Clip.antiAlias,
+            decoration: BoxDecoration(),
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  width: 100,
+                  child: Text(
+                    '로그아웃',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 15,
+                      fontFamily: 'Inter',
+                      fontWeight: FontWeight.w500,
+                      height: 1.5,
+                    ),
                   ),
                 ),
-              ),
-            ],
+                Icon(Icons.keyboard_arrow_right_outlined),
+              ],
+            ),
           ),
         ),
       ],
