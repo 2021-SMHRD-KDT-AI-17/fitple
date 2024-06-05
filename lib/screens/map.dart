078c14645f667fd292193ed67be59a1abd131409
SMHRD <sysy0514@gmail.com>
2024-06-05 09:00:27 +0900

    카카오 주소 검색 api 추가

--- lib/screens/map.dart
+++ lib/screens/map.dart
@@ -40,107 +40,95 @@ class NaverMapApp extends StatelessWidget {
   }
 }
 
-class NaverMapView extends StatelessWidget {
+class NaverMapView extends StatefulWidget {
   const NaverMapView({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    final Completer<NaverMapController> mapControllerCompleter = Completer();
+  _NaverMapViewState createState() => _NaverMapViewState();
+}
 
-    return NaverMap(
-      options: const NaverMapViewOptions(
-        indoorEnable: true,
-        locationButtonEnable: false,
-        consumeSymbolTapEvents: false,
-        initialCameraPosition: NCameraPosition(
-          target: NLatLng(35.15052, 126.9162),
-          zoom: 18,
-          bearing: 0,
-          tilt: 0,
+class _NaverMapViewState extends State<NaverMapView> {
+  final Completer<NaverMapController> _mapControllerCompleter = Completer();
+  final TextEditingController _postcodeController = TextEditingController();
+  final TextEditingController _addressController = TextEditingController();
+  final TextEditingController _addressDetailController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Expanded(
+          child: NaverMap(
+            options: NaverMapViewOptions(
+              indoorEnable: true,
+              locationButtonEnable: false,
+              consumeSymbolTapEvents: false,
+              initialCameraPosition: NCameraPosition(
+                target: NLatLng(35.15052, 126.9162),
+                zoom: 18,
+                bearing: 0,
+                tilt: 0,
+              ),
+            ),
+            onMapReady: (controller) async {
+              _mapControllerCompleter.complete(controller);
+              log("onMapReady", name: "onMapReady");
+              await _setInitialLocation(controller);
+            },
+          ),
         ),
-      ),
-      onMapReady: (controller) async {
-        mapControllerCompleter.complete(controller);
-        log("onMapReady", name: "onMapReady");
-        await _setInitialLocation(controller);
-      },
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          child: Column(
+            children: [
+              TextFormField(
+                controller: _postcodeController,
+                decoration: const InputDecoration(
+                  hintText: '우편번호',
+                ),
+                readOnly: true,
+              ),
+              const SizedBox(height: 10),
+              TextFormField(
+                controller: _addressController,
+                decoration: const InputDecoration(
+                  hintText: '기본주소',
+                ),
+                readOnly: true,
+              ),
+              const SizedBox(height: 10),
+              TextFormField(
+                textInputAction: TextInputAction.done,
+                controller: _addressDetailController,
+                decoration: const InputDecoration(
+                  hintText: '상세주소 입력',
+                ),
+              ),
+              const SizedBox(height: 10),
+              CupertinoButton(
+                onPressed: () => _searchAddress(context),
+                child: const Text('주소검색'),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 
   Future<void> _setInitialLocation(NaverMapController controller) async {
-    // 광주 동구 중앙로 196 스마트인재개발원 위치 좌표
+    // 초기 위치 설정 (스마트인재개발원 위치 좌표)
     final initialLocation = NLatLng(35.15052, 126.9162);
 
     // 해당 위치로 마커 추가
     final marker = NMarker(
-        id: 'smart',
-        position: const NLatLng(35.15052, 126.9162));
-    await controller.addOverlayAll({marker});
+      id: 'smart',
+      position: initialLocation,
+    );
+    await controller.addOverlay(marker);
 
     final onMarkerInfoWindow = NInfoWindow.onMarker(id: marker.info.id, text: "스마트인재개발원");
     marker.openInfoWindow(onMarkerInfoWindow);
-  }
-}
-
-class NaverMapView1 extends StatelessWidget {
-  const NaverMapView1({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    final Completer<NaverMapController> mapControllerCompleter = Completer();
-
-    return NaverMap(
-      options: const NaverMapViewOptions(
-        indoorEnable: true,
-        locationButtonEnable: true,
-        consumeSymbolTapEvents: true,
-        initialCameraPosition: NCameraPosition(
-          target: NLatLng(35.2204353, 126.8470647),
-          zoom: 18,
-          bearing: 0,
-          tilt: 0,
-        ),
-      ),
-      onMapReady: (controller) async {
-        mapControllerCompleter.complete(controller);
-        log("onMapReady", name: "onMapReady");
-        await _setInitialLocation(controller);
-      },
-    );
-  }
-
-  Future<void> _setInitialLocation(NaverMapController controller) async {
-    // 육체미 첨단점
-    final initialLocation = NLatLng(35.2204353, 126.8470647);
-
-    // 해당 위치로 마커 추가
-    final marker = NMarker(
-        id: '육체미',
-        position: const NLatLng(35.2204353, 126.8470647));
-    await controller.addOverlayAll({marker});
-
-    final onMarkerInfoWindow = NInfoWindow.onMarker(id: marker.info.id, text: "육체미 첨단점");
-    marker.openInfoWindow(onMarkerInfoWindow);
-  }
-}
-
-class KakaoAddressScreen extends StatelessWidget {
-  KakaoAddressScreen({super.key});
-
-  /// Form State
-  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-  Map<String, String> formData = {};
-
-  /// Controller
-  final TextEditingController _postcodeController = TextEditingController();
-  final TextEditingController _addressController = TextEditingController();
-  final TextEditingController _addressDetailController =
-  TextEditingController();
-
-  Widget _gap() {
-    return const SizedBox(
-      height: 10,
-    );
   }
 
   void _searchAddress(BuildContext context) async {
@@ -156,72 +144,36 @@ class KakaoAddressScreen extends StatelessWidget {
       _postcodeController.value = TextEditingValue(
         text: postcode,
       );
-      formData['postcode'] = postcode;
 
       final address = model.address ?? '';
       _addressController.value = TextEditingValue(
         text: address,
       );
-      formData['address'] = address;
 
       final buildingName = model.buildingName ?? '';
       _addressDetailController.value = TextEditingValue(
         text: buildingName,
       );
-      formData['address_detail'] = buildingName;
+
+      // 주소 검색 후 지도 위치 업데이트
+      if (address.isNotEmpty) {
+        final controller = await _mapControllerCompleter.future;
+        // 예시 좌표 (서울 시청)
+        final newLocation = NLatLng(37.5666102, 126.9783881);
+
+        final cameraUpdate = NCameraUpdate.scrollAndZoomTo(
+          target: newLocation,
+          zoom: 18,
+        ).setAnimation(
+          animation: NCameraAnimation.fly,
+          duration: const Duration(seconds: 2),
+        );
+
+        //await controller.animateCamera(cameraUpdate);
+
+        final newMarker = NMarker(id: 'newLocation', position: newLocation);
+        await controller.addOverlay(newMarker);
+      }
     }
   }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Kakao 주소검색 API'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Form(
-              key: _formKey,
-              child: Column(
-                children: <Widget>[
-                  TextFormField(
-                    controller: _postcodeController,
-                    decoration: const InputDecoration(
-                      hintText: '우편번호',
-                    ),
-                    readOnly: true,
-                  ),
-                  _gap(),
-                  TextFormField(
-                    controller: _addressController,
-                    decoration: const InputDecoration(
-                      hintText: '기본주소',
-                    ),
-                    readOnly: true,
-                  ),
-                  _gap(),
-                  TextFormField(
-                    textInputAction: TextInputAction.done,
-                    controller: _addressDetailController,
-                    decoration: const InputDecoration(
-                      hintText: '상세주소 입력',
-                    ),
-                  ),
-                  _gap(),
-                  CupertinoButton(
-                    onPressed: () => _searchAddress(context),
-                    child: const Text('주소검색'),
-                  )
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
 }
-
