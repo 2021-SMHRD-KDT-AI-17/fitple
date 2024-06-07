14f021e3ebdf119208d88dfe44706f18fd93db6f
SMHRD <a85193770@gmail.com>
2024-06-07 10:01:08 +0900

    커밋

---
+++ lib/DB/GymDB.dart
@@ -0,0 +1,23 @@
+import 'package:fitple/DB/DB.dart';
+
+// 헬스장 정보 리스트로 불러오기
+Future<List<Map<String, dynamic>>> loadTrainersWithGym() async {
+  final conn = await dbConnector();
+
+  final query = """
+    SELECT gym_name, gym_address, gym_phone_number, gym_picture, gym_time FROM fit_gym
+  """;
+
+  final results = await conn.execute(query);
+  await conn.close();
+
+  return results.rows.map((row) {
+    return {
+      "gym_name": row.colByName('gym_name'),
+      "gym_address": row.colByName('gym_address'),
+      "gym_phone_number": row.colByName('gym_phone_number'),
+      "gym_picture": row.colByName('gym_picture'),
+      "gym_time": row.colByName('gym_time'),
+    };
+  }).toList();
+}
