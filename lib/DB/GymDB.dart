b20879de9a52ee1349cde6a2bb6448297f582084
SMHRD <sysy0514@gmail.com>
2024-06-12 15:58:10 +0900

    트레이너 회원정보 수정 기능 + DB연동 home1페이지 헬스트레이너,헬스장 리스트 출력 기능 (이미지) 추가

--- lib/DB/GymDB.dart
+++ lib/DB/GymDB.dart
@@ -1,41 +1,62 @@
+import 'dart:convert';
 import 'dart:io';
+import 'dart:typed_data';
 
 import 'package:fitple/DB/DB.dart';
+import 'package:mysql_client/mysql_client.dart';
 
 // 헬스장 정보 리스트로 불러오기
 Future<List<Map<String, dynamic>>> loadGym() async {
   final conn = await dbConnector();
 
   final query = """
-    SELECT gym_name, gym_address, gym_phone_number, gym_picture, gym_time FROM fit_gym order by gym_point desc
+    SELECT gym_name, gym_address, gym_phone_number, gym_picture, gym_time
+    FROM fit_gym
+    ORDER BY gym_point DESC
   """;
 
   final results = await conn.execute(query);
   await conn.close();
 
   return results.rows.map((row) {
+    final pictureData = row.colByName('gym_picture');
+    Uint8List? pictureBytes;
+
+    if (pictureData != null && pictureData is String) {
+      try {
+        pictureBytes = base64Decode(pictureData);
+      } catch (e) {
+        print('Error decoding picture data: $e');
+      }
+    }
+
     return {
       "gym_name": row.colByName('gym_name'),
       "gym_address": row.colByName('gym_address'),
       "gym_phone_number": row.colByName('gym_phone_number'),
-      "gym_picture": row.colByName('gym_picture'),
+      "gym_picture": pictureBytes,
       "gym_time": row.colByName('gym_time'),
     };
   }).toList();
 }
-
 // 헬스장 등록
 Future<void> insertGym(String gymName, String gymAddress, String gymPhoneNumber, File? gymPicture, String gymStartTime, String gymEndTime) async {
   final conn = await dbConnector();
 
   try {
+    String? pictureBase64;
+    if (gymPicture != null) {
+      Uint8List pictureBytes = await gymPicture.readAsBytes();
+      pictureBase64 = base64Encode(pictureBytes);
+    }
+
     await conn.execute(
       "INSERT INTO fit_gym(gym_name, gym_address, gym_phone_number, gym_picture, gym_time) VALUES (:gym_name, :gym_address, :gym_phone_number, :gym_picture, :gym_time)",
       {
         "gym_name": gymName,
         "gym_address": gymAddress,
         "gym_phone_number": gymPhoneNumber,
-        "gym_picture": gymPicture != null ? gymPicture.readAsBytesSync() : null,
+        "gym_picture": pictureBase64,
         "gym_time": "$gymStartTime~$gymEndTime"
       },
     );
